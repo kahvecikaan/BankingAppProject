44d793c0bfe7b50b322f27d434983b4471bf72d7
KAAN KAHVECİ <kaank@BIS.NET>
2023-07-24 13:48:40 +0300

    Added new class library

---
+++ PasswordUtility/PasswordHelper/PasswordHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordUtility
+{
+    public class PasswordHelper
+    {
+        public static string HashPassword(string plainTextPassword)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(plainTextPassword));
+                StringBuilder builder = new StringBuilder();
+                for(int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2")); // the byte should be represented by a hexadecimal string of 2 char in length  
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
